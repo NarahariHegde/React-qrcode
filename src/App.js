3e5403dbaf680d3e5cdf35e26290041a3dad739b
Narahari Hegde <116895826+NarahariHegde@users.noreply.github.com>
2023-03-25 19:42:12 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,27 +1,11 @@
 import React from 'react'
 import './App.css';
 import Card from './Utils/Card';
-// import Qrcode from './Utils/Card';
-// import Colorcombination from './Screens/Colorcombination';
-// import Qrcode from './Screens/Qrcode';
-// import Home from './Component/Home';
-// import Single from "./Component/Single";
-// import Error from "./Component/Error";
-// import {BrowserRouter,Routes,Route} from 'react-router-dom';
+
 const App = () => {
   return (
     <>
       <div className="App">
-        {/* <BrowserRouter>
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="movie/:id" element={<Single/>} />
-            <Route path='*' element={<Error/>}/>
-          </Routes>
-        </BrowserRouter> */}
-
-
-
         <Card/>
       </div>
     </>
